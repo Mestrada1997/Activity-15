febc566f22ec2137b305bef5ba3718397abb332a
Malachi 84 <125697853+Mestrada1997@users.noreply.github.com>
2023-07-10 20:41:59 -0500

    Add files via upload

---
+++ ### Required Libraries ###.py
@@ -0,0 +1,151 @@
+### Required Libraries ###
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+### Functionality Helper Functions ###
+def parse_int(n):
+    """
+    Securely converts a non-integer value to integer.
+    """
+    try:
+        return int(n)
+    except ValueError:
+        return float("nan")
+
+
+def build_validation_result(is_valid, violated_slot, message_content):
+    """
+    Define a result message structured as Lex response.
+    """
+    if message_content is None:
+        return {"isValid": is_valid, "violatedSlot": violated_slot}
+
+    return {
+        "isValid": is_valid,
+        "violatedSlot": violated_slot,
+        "message": {"contentType": "PlainText", "content": message_content},
+    }
+
+### Dialog Actions Helper Functions ###
+def get_slots(intent_request):
+    """
+    Fetch all the slots and their values from the current intent.
+    """
+    return intent_request["currentIntent"]["slots"]
+
+
+def elicit_slot(session_attributes, intent_name, slots, slot_to_elicit, message):
+    """
+    Defines an elicit slot type response.
+    """
+
+    return {
+        "sessionAttributes": session_attributes,
+        "dialogAction": {
+            "type": "ElicitSlot",
+            "intentName": intent_name,
+            "slots": slots,
+            "slotToElicit": slot_to_elicit,
+            "message": message,
+        },
+    }
+
+
+def delegate(session_attributes, slots):
+    """
+    Defines a delegate slot type response.
+    """
+
+    return {
+        "sessionAttributes": session_attributes,
+        "dialogAction": {"type": "Delegate", "slots": slots},
+    }
+
+
+def close(session_attributes, fulfillment_state, message):
+    """
+    Defines a close slot type response.
+    """
+
+    response = {
+        "sessionAttributes": session_attributes,
+        "dialogAction": {
+            "type": "Close",
+            "fulfillmentState": fulfillment_state,
+            "message": message,
+        },
+    }
+
+    return response
+
+### Intents Handlers ###
+def recommend_portfolio(intent_request):
+    """
+    Performs dialog management and fulfillment for recommending a portfolio.
+    """
+
+    first_name = get_slots(intent_request)["firstName"]
+    age = get_slots(intent_request)["age"]
+    investment_amount = get_slots(intent_request)["investmentAmount"]
+    risk_level = get_slots(intent_request)["riskLevel"]
+    source = intent_request["invocationSource"]
+
+    # Validate age and investment amount
+    if age <= 0 or age >= 65:
+        return build_validation_result(
+            False,
+            "age",
+            "The age should be greater than zero and less than 65."
+        )
+
+    if investment_amount < 5000:
+        return build_validation_result(
+            False,
+            "investmentAmount",
+            "The investment amount should be equal to or greater than $5000."
+        )
+    # Define the investment recommendations based on risk level
+    recommendations = {
+        "none": "100% bonds (AGG), 0% equities (SPY)",
+        "low": "60% bonds (AGG), 40% equities (SPY)",
+        "medium": "40% bonds (AGG), 60% equities (SPY)",
+        "high": "20% bonds (AGG), 80% equities (SPY)"
+    }
+
+    # Generate the investment recommendation message
+    recommendation = f"Hello {first_name}, based on your risk level ({risk_level}), I recommend the following investment allocation:\n\n"
+    recommendation += recommendations[risk_level.lower()]
+
+# Return the recommendation as the fulfillment response
+    return close(
+        intent_request["sessionAttributes"],
+        "Fulfilled",
+        {
+            "contentType": "PlainText",
+            "content": recommendation
+        }
+    )
+
+
+### Intents Dispatcher ###
+def dispatch(intent_request):
+    """
+    Called when the user specifies an intent for this bot.
+    """
+
+    intent_name = intent_request["currentIntent"]["name"]
+
+    # Dispatch to bot's intent handlers
+    if intent_name == "recommendPortfolio":
+        return recommend_portfolio(intent_request)
+
+    raise Exception("Intent with name " + intent_name + " not supported")
+
+### Main Handler ###
+def lambda_handler(event, context):
+    """
+    Route the incoming request based on intent.
+    The JSON body of the request is provided in the event slot.
+    """
+
+    return dispatch(event)
